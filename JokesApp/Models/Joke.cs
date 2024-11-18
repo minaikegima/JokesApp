ea05603d78cf4e7317edc4959770af95acf29f4e
Ikegim <Ikegim@ADA-MINARO-0356>
2024-11-18 15:23:15 +0000

    Add project files.

---
+++ JokesApp/Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+    }
+}
